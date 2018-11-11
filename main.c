e10974c4d2e0217c0d7a801209f64064c63e4fcb
tuhage <tuhage@gmail.com>
2018-11-11 06:48:17 +0300

    v1.0

--- main.c
+++ main.c
@@ -4,8 +4,7 @@
 #define BOYUT 256
 #define BOYUT2 500
 
-char olaylar[BOYUT2];
-int saniye=0;
+
 struct KAPI{
     char isim[4];
     int giris_sayisi;
@@ -29,16 +28,22 @@ struct DEVRE {
 
 };
 
+struct DEVRE *devre ;
+char olaylar[BOYUT2];
+int saniye=0;
 
-int komutcalistir(const char *komut, struct DEVRE *devre);
-void kapilarisirala(struct DEVRE *devre);
-int giris_var_mi(const struct  DEVRE *devre,char giris);
-void devresimulasyonu(struct DEVRE *devre,char *degisecekuclar,int durum);
-void kapiyicalistir(struct KAPI *kapi);
+int komutcalistir(const char *komut);
+void kapilarisirala();
+int giris_var_mi(char giris);
+int uc_var_mi(char giris);
+void devresimulasyonu(char *degisecekuclar,int durum);
+int kapiyicalistir(struct KAPI *kapi);
 int indexbul(char *dizi,char a);
+void degerdegistir(char isim,int yenideger);
 
 int main() {
-    struct DEVRE *devre=malloc(sizeof(struct DEVRE)) ;
+
+    devre=malloc(sizeof(struct DEVRE));
 
     devre->giris_sayisi= 3;
     devre->kapi_sayisi=3;
@@ -105,14 +110,14 @@ int main() {
     devre->kapilar[1].cikis_degeri=1;
     devre->kapilar[2].cikis_degeri=0;
 
-    kapilarisirala(devre);
+    kapilarisirala();
 
 
 
 
 
 
-    printf("%c  %d   %d",devre->giris_isimleri[0],devre->giris_degerleri[1],devre->giris_degerleri[2]);
+
     char *komut;
     komut=(char *)malloc(sizeof(char)*BOYUT);
     do {
@@ -120,7 +125,7 @@ int main() {
         printf("\n>");
         fgets(komut,BOYUT,stdin);
         komut[strlen(komut)-1]='\0'; // Stringin sonuna sonlandıra karakteri ekler.
-        if(komutcalistir(komut,devre)){
+        if(komutcalistir(komut)){
 
         }
         else
@@ -142,7 +147,7 @@ int main() {
 
 
 
-int komutcalistir(const char *komut, struct DEVRE *devre){
+int komutcalistir(const char *komut){
 
 if(komut[0]=='Y'||komut[0]=='y'){
     /*
@@ -203,7 +208,7 @@ else if(komut[0]=='H'||komut[0]=='h') //verilen uçları lojik-1 yapar.
 
         if(komut[i]==' ')continue; // bosluklar atlanir.
 
-        kontrol=giris_var_mi(devre,komut[i]); //giris ucunun devrede bulunup bulunmadığını konrol eden fonksiyon
+        kontrol=giris_var_mi(komut[i]); //giris ucunun devrede bulunup bulunmadığını konrol eden fonksiyon
 
         if(kontrol==0)
         {
@@ -228,18 +233,57 @@ else if(komut[0]=='H'||komut[0]=='h') //verilen uçları lojik-1 yapar.
     }
 
 
-    devresimulasyonu(devre,degisecekuclar,1);
+    devresimulasyonu(degisecekuclar,1);
 
 
 
 
     return 1;
 }
-else if(komut[0]=='L'||komut[0]=='l'){
+else if(komut[0]=='L'||komut[0]=='l'){  // verilen uçları lojik 0 yapar.
+
+    int uc_sayisi=0,kontrol=0;
+
+    if(strlen(komut)==1){
+        printf("Yetersiz parametre girdiniz.");
+        return 0;
+    }
+
+    for (int i = 1; i <strlen(komut) ; ++i) { //uc sayisi bulunur.
+
+        if(komut[i]==' ')continue; // bosluklar atlanir.
+
+        kontrol=giris_var_mi(komut[i]); //giris ucunun devrede bulunup bulunmadığını konrol eden fonksiyon
+
+        if(kontrol==0)
+        {
+            printf("Girdiginiz giris ucu devrede bulunmamaktadir.");
+            return 0;
+        }
+        uc_sayisi++;
+
+    }
+
+    char degisecekuclar[uc_sayisi];int j=0; //uc sayisi kadar eleman tutacak bir degisecekuclar dizisi olusturulur.
+    degisecekuclar[uc_sayisi]='\0'; //strlen fonksiyonunun kullanımı için son karaktere '\0'(sonlandırma karakteri) değeri atandı.
+    for (int i = 1; i <strlen(komut) ; ++i) //degisecekuclar dizisi komutta verilen uclar ile doldurur.
+    {
+
+
+        if(komut[i]==' ')continue;
+
+        degisecekuclar[j]=komut[i];
+        j++;
+
+    }
+
+
+    devresimulasyonu(degisecekuclar,0);
+
 
     return 1;
 }
-else if(komut[0]=='S'||komut[0]=='s'){
+else if(komut[0]=='S'||komut[0]=='s'){ //gerçekleşen olayları yazar.
 
     printf("%s",olaylar);
     strcpy(olaylar,"");
@@ -247,6 +291,55 @@ else if(komut[0]=='S'||komut[0]=='s'){
     return 1;
 }
 else if(komut[0]=='G'||komut[0]=='g'){
+
+    int uc_sayisi=0,kontrol=0;
+
+    if(komut[1]=='*'&&strlen(komut)>2){
+        return 0;
+    }
+    else if(komut[1]=='*'){
+
+        /*hepsini yazacak*/
+        return 1;
+    }
+
+
+    if(strlen(komut)==1){
+        printf("Yetersiz parametre girdiniz.");
+        return 0;
+    }
+
+    for (int i = 1; i <strlen(komut) ; ++i) { //uc sayisi bulunur.
+
+        if(komut[i]==' ')continue; // bosluklar atlanir.
+
+        kontrol=uc_var_mi(komut[i]); //giris ucunun devrede bulunup bulunmadığını konrol eden fonksiyon
+
+        if(kontrol==0)
+        {
+            printf("Girdiginiz uc devrede bulunmamaktadir.");
+            return 0;
+        }
+        uc_sayisi++;
+
+    }
+
+    char uclar[uc_sayisi];int j=0; //uc sayisi kadar eleman tutacak bir uclar dizisi olusturulur.
+    uclar[uc_sayisi]='\0'; //strlen fonksiyonunun kullanımı için son karaktere '\0'(sonlandırma karakteri) değeri atandı.
+    for (int i = 1; i <strlen(komut) ; ++i) //degisecekuclar dizisi komutta verilen uclar ile doldurur.
+    {
+
+
+        if(komut[i]==' ')continue;
+
+        uclar[j]=komut[i];
+        j++;
+
+    }
+
+
+
+
 
     return 1;
 }
@@ -264,7 +357,7 @@ if(strlen(komut)>1)return 0;
 
 }
 
-void kapilarisirala(struct DEVRE *devre){
+void kapilarisirala(){
 
     struct KAPI temp;
 
@@ -289,7 +382,7 @@ void kapilarisirala(struct DEVRE *devre){
 }
 
 
-int giris_var_mi(const struct  DEVRE *devre,char giris){
+int giris_var_mi(char giris){
 
     int kontrol=0;
 
@@ -303,8 +396,23 @@ int giris_var_mi(const struct  DEVRE *devre,char giris){
 
 }
 
-void devresimulasyonu(struct DEVRE *devre,char *degisecekuclar,int durum){
- char temp[BOYUT2];
+int uc_var_mi(char giris){
+
+    int kontrol=0;
+    for (int i = 0; i <devre->giris_sayisi ; ++i) {
+        if(devre->cikis_isimleri[i]==giris)kontrol++;
+    }
+    for (int j = 0; j <devre->kapi_sayisi ; ++j) {
+        if(devre->kapilar[j].cikis_isim==giris)kontrol++;
+    }
+
+    return kontrol;
+
+
+}
+
+void devresimulasyonu(char *degisecekuclar,int durum){
+ char temp[BOYUT2];int kontrol=0;
 
     if(durum==1) {
 
@@ -312,12 +420,49 @@ void devresimulasyonu(struct DEVRE *devre,char *degisecekuclar,int durum){
 
             sprintf(temp,"%dns: %c %d->%d\n", saniye,devre->giris_isimleri[indexbul(devre->giris_isimleri,degisecekuclar[i])],(devre->giris_degerleri[indexbul(devre->giris_isimleri,degisecekuclar[i])]),1);
             strcat(olaylar,temp);
-            saniye++;
-            devre->giris_degerleri[indexbul(devre->giris_isimleri,degisecekuclar[i])]=1;
-        }
+          //  saniye++;  ilk atamada saniye değeri hep 0 fakat bu yorum satırı kaldırılırsa değer arttırılır.
+            degerdegistir(degisecekuclar[i],1);
+
+        }saniye=1;// yukarıdaki saniye++; kısmındaki yorum satırı kaldırılır ise bu eşiklik silinmelidir.
+
+
         for (int j = 0; j <devre->kapi_sayisi ; ++j) {
 
-                kapiyicalistir(&(devre->kapilar[j]));
+            if(kapiyicalistir(&(devre->kapilar[j])))kontrol++;
+
+            if(j==devre->kapi_sayisi-1)
+                if(kontrol!=0){
+                    j=0;
+                    kontrol=0;
+
+                }
+
+        }
+
+    }
+    else
+    {
+
+        for (int i = 0; i < strlen(degisecekuclar); ++i) {
+
+            sprintf(temp,"%dns: %c %d->%d\n", saniye,devre->giris_isimleri[indexbul(devre->giris_isimleri,degisecekuclar[i])],(devre->giris_degerleri[indexbul(devre->giris_isimleri,degisecekuclar[i])]),0);
+            strcat(olaylar,temp);
+            //  saniye++;  ilk atamada saniye değeri hep 0 fakat bu yorum satırı kaldırılırsa değer arttırılır.
+            degerdegistir(degisecekuclar[i],0);
+
+        }  saniye=1;// yukarıdaki saniye++; kısmındaki yorum satırı kaldırılır ise bu eşiklik silinmelidir.
+
+
+        for (int j = 0; j <devre->kapi_sayisi ; ++j) {
+
+            if(kapiyicalistir(&(devre->kapilar[j])))kontrol++;
+
+            if(j==devre->kapi_sayisi-1)
+                if(kontrol!=0){
+                    j=0;
+                    kontrol=0;
+
+                }
 
         }
 
@@ -327,19 +472,48 @@ void devresimulasyonu(struct DEVRE *devre,char *degisecekuclar,int durum){
 
 }
 
-void kapiyicalistir(struct KAPI *kapi){
+int kapiyicalistir(struct KAPI *kapi){ // kapıyı çalışıtırır mantıksal işlemleri yapar ve kapının çıkış değerini değiştirir.
+
+    char temp[BOYUT2];int sonuc=kapi->giris_degerleri[0], deger=kapi->cikis_degeri;
 
 
 if(strcmp(kapi->isim,"AND")==0||strcmp(kapi->isim,"and")==0){
 
+    for (int i = 1; i <kapi->giris_sayisi ; ++i) {
+        sonuc*=kapi->giris_degerleri[i];
+    }
 
 
+
+    if(sonuc==0)degerdegistir(kapi->cikis_isim,0);
+    else degerdegistir(kapi->cikis_isim,1);
+
+    if(deger==kapi->cikis_degeri)return 0;
+
+
+    sprintf(temp,"%dns: %c %d->%d\n", saniye,kapi->cikis_isim,deger,kapi->cikis_degeri);
+    strcat(olaylar,temp);
+    saniye++;
 
 
 }
 else if(strcmp(kapi->isim,"NOR")==0||strcmp(kapi->isim,"nor")==0){
 
 
+    for (int i = 1; i <kapi->giris_sayisi ; ++i) {
+        sonuc+=kapi->giris_degerleri[i];
+    }
+
+
+
+    if(sonuc==0)degerdegistir(kapi->cikis_isim,1);
+    else degerdegistir(kapi->cikis_isim,0);
+
+    if(deger==kapi->cikis_degeri)return 0;
+
+    sprintf(temp,"%dns: %c %d->%d\n", saniye,kapi->cikis_isim,deger,kapi->cikis_degeri);
+    strcat(olaylar,temp);
+    saniye++;
 
 
 
@@ -347,6 +521,18 @@ else if(strcmp(kapi->isim,"NOR")==0||strcmp(kapi->isim,"nor")==0){
 else if(strcmp(kapi->isim,"OR")==0||strcmp(kapi->isim,"or")==0){
 
 
+    for (int i = 1; i <kapi->giris_sayisi ; ++i) {
+        sonuc+=kapi->giris_degerleri[i];
+    }
+
+    if(sonuc==0)degerdegistir(kapi->cikis_isim,0);
+    else degerdegistir(kapi->cikis_isim,1);
+
+    if(deger==kapi->cikis_degeri)return 0;
+
+    sprintf(temp,"%dns: %c %d->%d\n", saniye,kapi->cikis_isim,deger,kapi->cikis_degeri);
+    strcat(olaylar,temp);
+    saniye++;
 
 
 
@@ -354,13 +540,31 @@ else if(strcmp(kapi->isim,"OR")==0||strcmp(kapi->isim,"or")==0){
 else if(strcmp(kapi->isim,"XOR")==0||strcmp(kapi->isim,"xor")==0){
 
 
+if(kapi->giris_degerleri[0]==kapi->giris_degerleri[1])degerdegistir(kapi->cikis_isim,0);
+else degerdegistir(kapi->cikis_isim,1);
 
-
+    if(deger==kapi->cikis_degeri)return 0;
+    sprintf(temp,"%dns: %c %d->%d\n", saniye,kapi->cikis_isim,deger,kapi->cikis_degeri);
+    strcat(olaylar,temp);
+    saniye++;
 
 }
 else if(strcmp(kapi->isim,"NAND")==0||strcmp(kapi->isim,"nand")==0){
 
 
+    for (int i = 1; i <kapi->giris_sayisi ; ++i) {
+        sonuc*=kapi->giris_degerleri[i];
+    }
+
+    if(sonuc==0)degerdegistir(kapi->cikis_isim,1);
+    else degerdegistir(kapi->cikis_isim,0);
+
+    if(deger==kapi->cikis_degeri)return 0;
+
+        //printf("\n %c=%d %c=%d \nsonuc=%d\n",kapi->giris_isimleri[0],kapi->giris_degerleri[0],kapi->giris_isimleri[1],kapi->giris_degerleri[1],sonuc);
+    sprintf(temp,"%dns: %c %d->%d\n", saniye,kapi->cikis_isim,deger,kapi->cikis_degeri);
+    strcat(olaylar,temp);
+    saniye++;
 
 
 
@@ -368,12 +572,29 @@ else if(strcmp(kapi->isim,"NAND")==0||strcmp(kapi->isim,"nand")==0){
 else if(strcmp(kapi->isim,"NOT")==0||strcmp(kapi->isim,"not")==0){
 
 
+if(kapi->giris_degerleri[0]==0)degerdegistir(kapi->cikis_isim,1);
+else degerdegistir(kapi->cikis_isim,0);
+
+    if(deger==kapi->cikis_degeri)return 0;
+    sprintf(temp,"%dns: %c %d->%d\n", saniye,kapi->cikis_isim,deger,kapi->cikis_degeri);
+    strcat(olaylar,temp);
+    saniye++;
+
+}
+else if(strcmp(kapi->isim,"EXOR")==0||strcmp(kapi->isim,"exor")==0){
 
 
+    if(kapi->giris_degerleri[0]==kapi->giris_degerleri[1])degerdegistir(kapi->cikis_isim,0);
+    else degerdegistir(kapi->cikis_isim,1);
+
+    if(deger==kapi->cikis_degeri)return 0;
+    sprintf(temp,"%dns: %c %d->%d\n", saniye,kapi->cikis_isim,deger,kapi->cikis_degeri);
+    strcat(olaylar,temp);
+    saniye++;
 
 }
 
-
+return 1;
 
 
 }
@@ -384,4 +605,29 @@ int indexbul(char *dizi,char a){
         if(dizi[i]==a)return i;
     }
     return -1;
+}
+
+void degerdegistir(char isim,int yenideger){
+
+
+    for (int i = 0; i <devre->kapi_sayisi ; ++i) {
+        if(devre->kapilar[i].giris_isimleri[indexbul(devre->kapilar[i].giris_isimleri,isim)]!=-1)
+        devre->kapilar[i].giris_degerleri[indexbul(devre->kapilar[i].giris_isimleri,isim)]=yenideger;
+
+        if(devre->kapilar[i].cikis_isim==isim)devre->kapilar[i].cikis_degeri=yenideger;
+    }
+
+    for (int j = 0; j <devre->giris_sayisi ; ++j) {
+        if(devre->giris_isimleri[indexbul(devre->giris_isimleri,isim)]!=-1)
+            devre->giris_degerleri[indexbul(devre->giris_isimleri,isim)]=yenideger;
+
+    }
+
+    for (int j = 0; j <devre->cikis_sayisi ; ++j) {
+        if(devre->cikis_isimleri[indexbul(devre->cikis_isimleri,isim)]!=-1)
+            devre->cikis_degerleri[indexbul(devre->cikis_isimleri,isim)]=yenideger;
+
+    }
+
+
 }
